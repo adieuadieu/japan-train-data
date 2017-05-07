95d82a3e0ad42bc8b3c14c39a478086f38674157
Marco Lüthy <marco@luethy.net>
2017-05-07 20:06:18 +0800

    feat: get the package working right

--- README.md
+++ README.md
@@ -2,16 +2,30 @@
 
 Choo choo.
 
-A recursive object of train 🚂  data for Japan 🇯🇵 including translations & station geocoding _and_ a tool to generate it.
+A recursive object of train data for Japan including translations & station geocoding _and_ a tool to generate it.
 
+## Install
+
+`yarn add japan-train-data`
+
+## Usage
+
+```js
+import { prefectures, lines, stations } from 'japan-train-data'
+
+console.log(...prefectures)
+console.log(...lines)
+console.log(...stations)
+```
 
 ### Known Issues
 
 1. Some of the translations are clearly crap.
 1. In many cases, transliteration would be better than translation, but there's no API? Suggestions?
 
+
 ### Todo
 
-[ ] npm package
-[ ] unit tests
-[ ] ci integrations
+- [X] npm package
+- [ ] unit tests
+- [ ] ci integrations
--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 import data from '../data/raw-data.json'
 
-export default data
+// basically the only time that i've ever used forEach instead of map
+// turn 'data' into a recursive structure
+data.forEach((prefecture) => {
+  prefecture.lines.forEach((line) => {
+    line.prefecture = prefecture
+    line.stations.forEach((station) => {
+      station.prefecture = prefecture
+
+      station.lines.forEach(({ id: lineId }) => {
+        // link this stations lines to the lines by id
+      })
+    })
+  })
+})
+
+export const prefectures = data
+
+export const lines = data.reduce(
+  (reducedPrefectures, prefecture) => [
+    ...reducedPrefectures,
+    ...prefecture.lines,
+  ],
+  []
+)
+
+export const stations = data.reduce(
+  (reducedPrefectures, prefecture) => [
+    ...reducedPrefectures,
+    ...prefecture.lines.reduce(
+      (reducedLines, line) => [...reducedLines, ...line.stations],
+      []
+    ),
+  ],
+  []
+)
